1bad71392507bf2510302d5a6f244bcbbb9760cf
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 15:38:15 -0600

    Update Administrador.java

--- siup/src/main/java/com/universidad/model/entities/Administrador.java
+++ siup/src/main/java/com/universidad/model/entities/Administrador.java
@@ -1,23 +1,24 @@
 package com.universidad.model.entities;
 
+import com.universidad.model.enums.TipoUsuario;
+
 public class Administrador extends Usuario {
     private String numeroEmpleado;
 
     public Administrador() {
-        this.setTipo(TipoUsuario.ADMIN);
+        setTipo(TipoUsuario.ADMIN);
     }
+
     public Administrador(int id, String nombre, String correo, String contrasena, String numeroEmpleado) {
         super(id, nombre, correo, contrasena, TipoUsuario.ADMIN);
         this.numeroEmpleado = numeroEmpleado;
     }
-    public String getNumeroEmpleado() {
-        return this.numeroEmpleado;
-    }
-    public void setNumeroEmpleado(String numeroEmpleado) {
-        this.numeroEmpleado = numeroEmpleado;
-    }
+
+    public String getNumeroEmpleado() { return numeroEmpleado; }
+    public void setNumeroEmpleado(String numeroEmpleado) { this.numeroEmpleado = numeroEmpleado; }
+
+    @Override
     public String toString() {
-        String var10000 = this.getNombre();
-        return "Admin: " + var10000 + " [" + this.numeroEmpleado + "]";
+        return "Admin: " + getNombre() + " [" + numeroEmpleado + "]";
     }
 }
