1c0ed879efa0cf2cde6407e7c38d379c50b59f31
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:21:34 -0600

    Update Inscripcion.java

--- siup/src/main/java/com/universidad/model/entities/Inscripcion.java
+++ siup/src/main/java/com/universidad/model/entities/Inscripcion.java
@@ -1,4 +1,21 @@
 package main.java.com.universidad.model.entities;
 
 public class Inscripcion {
+  public class Inscripcion {
+    private int id;
+    private int idAlumno;
+    private int idGrupo;
+    private String nombreAlumno;
+    private String matricula;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int id, int idAlumno, int idGrupo) {
+        this.id = id;
+        this.idAlumno = idAlumno;
+        this.idGrupo = idGrupo;
+    }
+
+    
 }
