3ff34cab853b72162dcb8d42d8608450583604d9
gabocrk <gabocrk@gmail.com>
2026-06-09 10:15:21 -0600

    Update Main.java

--- siup/src/main/java/com/universidad/Main.java
+++ siup/src/main/java/com/universidad/Main.java
@@ -1,4 +1,9 @@
-package main.java.com.universidad;
+package com.universidad;
+
+import javafx.application.Application;
 
 public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
 }
