5c86d51c54a603cc4a687d6f50816f6de77e0e27
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:27:42 -0600

    Update Materia.java

--- siup/src/main/java/com/universidad/model/entities/Materia.java
+++ siup/src/main/java/com/universidad/model/entities/Materia.java
@@ -17,4 +17,40 @@ public class Materia {
         this.idCarrera = idCarrera;
         this.semestre = semestre;
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdCarrera() {
+        return this.idCarrera;
+    }
+
+     public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public int getSemestre() {
+        return this.semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getNombreCarrera() {
+        return this.nombreCarrera;
+    }
 }
