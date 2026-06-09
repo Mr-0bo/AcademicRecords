94bdc969598f267850933634656d2d59f058b073
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:14:26 -0600

    Update Grupo.java

--- siup/src/main/java/com/universidad/model/entities/Grupo.java
+++ siup/src/main/java/com/universidad/model/entities/Grupo.java
@@ -1,4 +1,14 @@
 package main.java.com.universidad.model.entities;
 
 public class Grupo {
+      public class Grupo {
+    private int id;
+    private int idMateria;
+    private int idProfesor;
+    private String periodo;
+    private String nombreMateria;
+    private String nombreProfesor;
+
+    public Grupo() {
+    }
 }
