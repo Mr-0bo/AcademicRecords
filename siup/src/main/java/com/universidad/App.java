dfb0b3d4b72e22d6cc2e8af8fe64cd5177511438
gabocrk <gabocrk@gmail.com>
2026-06-09 10:53:54 -0600

    Update App.java

--- siup/src/main/java/com/universidad/App.java
+++ siup/src/main/java/com/universidad/App.java
@@ -1,1 +1,8 @@
 
+package com.universidad;
+
+public class App extends javafx.application.Application {
+    public App() { /* compiled code */ }
+
+    public void start(javafx.stage.Stage primaryStage) { /* compiled code */ }
+}
