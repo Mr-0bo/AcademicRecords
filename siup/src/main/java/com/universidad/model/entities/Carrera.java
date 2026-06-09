c9b9b0decc1a2d7ad71e7351212d32d6ad3aa2de
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:08:12 -0600

    Add getter and setter methods for Carrera fields

--- siup/src/main/java/com/universidad/model/entities/Carrera.java
+++ siup/src/main/java/com/universidad/model/entities/Carrera.java
@@ -12,4 +12,20 @@ public class Carrera {
         this.id = id;
         this.nombre = nombre;
     }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
