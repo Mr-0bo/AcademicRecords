51e3bd0643594f7889c0dfcce364f915f598570f
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:26:42 -0600

    Update Materia.java

--- siup/src/main/java/com/universidad/model/entities/Materia.java
+++ siup/src/main/java/com/universidad/model/entities/Materia.java
@@ -1,4 +1,20 @@
 package main.java.com.universidad.model.entities;
 
 public class Materia {
+  public class Materia {
+    private int id;
+    private String nombre;
+    private int idCarrera;
+    private int semestre;
+    private String nombreCarrera;
+
+    public Materia() {
+    }
+
+    public Materia(int id, String nombre, int idCarrera, int semestre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.idCarrera = idCarrera;
+        this.semestre = semestre;
+    }
 }
