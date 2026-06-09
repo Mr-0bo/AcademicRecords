fbda8179568f4d6c99a1ecfac9b10d2acec2e0a6
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 17:07:18 -0600

    Update Carrera.java

--- siup/src/main/java/com/universidad/model/entities/Carrera.java
+++ siup/src/main/java/com/universidad/model/entities/Carrera.java
@@ -4,31 +4,19 @@ public class Carrera {
     private int id;
     private String nombre;
 
-    public Carrera() {
-    }
+    public Carrera() {}
 
     public Carrera(int id, String nombre) {
         this.id = id;
         this.nombre = nombre;
     }
 
-    public int getId() {
-        return this.id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getNombre() {
-        return this.nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-     public String toString() {
-        return this.nombre;
-    }
+    @Override
+    public String toString() { return nombre; }
 }
