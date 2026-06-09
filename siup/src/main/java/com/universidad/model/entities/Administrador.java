b579377714b6009a57a859272f072520a0b6ef71
IanSigler <iancarlosigler@gmail.com>
2026-06-09 08:49:53 -0600

    Extend Administrador class from Usuario

--- siup/src/main/java/com/universidad/model/entities/Administrador.java
+++ siup/src/main/java/com/universidad/model/entities/Administrador.java
@@ -1,4 +1,10 @@
 package main.java.com.universidad.model.entities;
 
 public class Administrador {
+  public class Administrador extends Usuario {
+    private String numeroEmpleado;
+
+    public Administrador() {
+        this.setTipo(TipoUsuario.ADMIN);
+    }
 }
