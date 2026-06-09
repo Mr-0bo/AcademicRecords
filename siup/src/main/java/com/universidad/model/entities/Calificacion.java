dc140637386df47408617abcac8c7e8b747f2068
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:03:21 -0600

    Update Calificacion.java

--- siup/src/main/java/com/universidad/model/entities/Calificacion.java
+++ siup/src/main/java/com/universidad/model/entities/Calificacion.java
@@ -1,4 +1,18 @@
 package main.java.com.universidad.model.entities;
 
 public class Calificacion {
+  public class Calificacion {
+    private int id;
+    private int idInscripcion;
+    private double parcial1;
+    private double parcial2;
+    private double parcial3;
+    private double actividades;
+    private double proyecto;
+    private double promedioFinal;
+    private boolean aprobado;
+    private String nombreAlumno;
+    private String matricula;
+
+    
 }
