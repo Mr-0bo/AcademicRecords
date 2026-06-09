7e87fa8e4f2f8e7c60ac81d34db6109bb85f1f62
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 17:06:05 -0600

    Update Alumno.java

--- siup/src/main/java/com/universidad/model/entities/Alumno.java
+++ siup/src/main/java/com/universidad/model/entities/Alumno.java
@@ -5,40 +5,30 @@ import com.universidad.model.enums.TipoUsuario;
 public class Alumno extends Usuario {
     private String matricula;
     private int idCarrera;
-    private String nombreCarrera;
+    private String nombreCarrera; // campo auxiliar para mostrar en UI
 
     public Alumno() {
-        this.setTipo(TipoUsuario.ALUMNO);
+        setTipo(TipoUsuario.ALUMNO);
     }
-    public Alumno(int id, String nombre, String correo, String contrasena, String matricula, int idCarrera) {
+
+    public Alumno(int id, String nombre, String correo, String contrasena,
+                  String matricula, int idCarrera) {
         super(id, nombre, correo, contrasena, TipoUsuario.ALUMNO);
         this.matricula = matricula;
         this.idCarrera = idCarrera;
     }
-    public String getMatricula() {
-        return this.matricula;
-    }
 
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
 
-    public int getIdCarrera() {
-        return this.idCarrera;
-    }
-    public void setIdCarrera(int idCarrera) {
-        this.idCarrera = idCarrera;
-    }
+    public int getIdCarrera() { return idCarrera; }
+    public void setIdCarrera(int idCarrera) { this.idCarrera = idCarrera; }
 
-    public String getNombreCarrera() {
-        return this.nombreCarrera;
-    }
+    public String getNombreCarrera() { return nombreCarrera; }
+    public void setNombreCarrera(String nombreCarrera) { this.nombreCarrera = nombreCarrera; }
 
-    public void setNombreCarrera(String nombreCarrera) {
-        this.nombreCarrera = nombreCarrera;
-    }
+    @Override
     public String toString() {
-        String var10000 = this.getNombre();
-        return var10000 + " (" + this.matricula + ")";
+        return getNombre() + " (" + matricula + ")";
     }
 }
