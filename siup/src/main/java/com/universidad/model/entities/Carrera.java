b0be1bb657c43f374aa30a9ef939bd48f4d85ff7
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:07:06 -0600

    Update Carrera.java

--- siup/src/main/java/com/universidad/model/entities/Carrera.java
+++ siup/src/main/java/com/universidad/model/entities/Carrera.java
@@ -1,4 +1,15 @@
 package main.java.com.universidad.model.entities;
 
 public class Carrera {
+  public class Carrera {
+    private int id;
+    private String nombre;
+
+    public Carrera() {
+    }
+
+    public Carrera(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
 }
