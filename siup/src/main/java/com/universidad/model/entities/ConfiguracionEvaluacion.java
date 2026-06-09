3e45a08125aaa591e526c8f995ff098e556501a6
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 17:07:44 -0600

    Update ConfiguracionEvaluacion.java

--- siup/src/main/java/com/universidad/model/entities/ConfiguracionEvaluacion.java
+++ siup/src/main/java/com/universidad/model/entities/ConfiguracionEvaluacion.java
@@ -1,18 +1,26 @@
 package com.universidad.model.entities;
 
+/**
+ * Define los porcentajes de evaluación de una materia.
+ * Los tres porcentajes DEBEN sumar 100.
+ */
 public class ConfiguracionEvaluacion {
     private int id;
     private int idMateria;
-    private double porcentajeParciales;
-    private double porcentajeActividades;
-    private double porcentajeProyecto;
-    private double calificacionMinima;
-    private int faltasMaximas;
+    private double porcentajeParciales;   // ej. 60.0
+    private double porcentajeActividades; // ej. 30.0
+    private double porcentajeProyecto;    // ej. 10.0
+    private double calificacionMinima;    // ej. 6.0
+    private int faltasMaximas;            // ej. 3
 
-    public ConfiguracionEvaluacion() {
-    }
+    public ConfiguracionEvaluacion() {}
 
-     public ConfiguracionEvaluacion(int id, int idMateria, double porcentajeParciales, double porcentajeActividades, double porcentajeProyecto, double calificacionMinima, int faltasMaximas) {
+    public ConfiguracionEvaluacion(int id, int idMateria,
+                                   double porcentajeParciales,
+                                   double porcentajeActividades,
+                                   double porcentajeProyecto,
+                                   double calificacionMinima,
+                                   int faltasMaximas) {
         this.id = id;
         this.idMateria = idMateria;
         this.porcentajeParciales = porcentajeParciales;
@@ -22,64 +30,32 @@ public class ConfiguracionEvaluacion {
         this.faltasMaximas = faltasMaximas;
     }
 
+    /** Valida que los porcentajes sumen exactamente 100 */
     public boolean esValida() {
-        double suma = this.porcentajeParciales + this.porcentajeActividades + this.porcentajeProyecto;
-        return Math.abs(suma - (double)100.0F) < 0.001;
+        double suma = porcentajeParciales + porcentajeActividades + porcentajeProyecto;
+        return Math.abs(suma - 100.0) < 0.001;
     }
 
-        public int getId() {
-        return this.id;
-    }
+    // ── Getters y Setters ────────────────────────────────────────────────────
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public int getIdMateria() {
-        return this.idMateria;
-    }
+    public int getIdMateria() { return idMateria; }
+    public void setIdMateria(int idMateria) { this.idMateria = idMateria; }
 
-    public void setIdMateria(int idMateria) {
-        this.idMateria = idMateria;
-    }
+    public double getPorcentajeParciales() { return porcentajeParciales; }
+    public void setPorcentajeParciales(double v) { this.porcentajeParciales = v; }
 
-    public double getPorcentajeParciales() {
-        return this.porcentajeParciales;
-    }
+    public double getPorcentajeActividades() { return porcentajeActividades; }
+    public void setPorcentajeActividades(double v) { this.porcentajeActividades = v; }
 
-    public void setPorcentajeParciales(double v) {
-        this.porcentajeParciales = v;
-    }
+    public double getPorcentajeProyecto() { return porcentajeProyecto; }
+    public void setPorcentajeProyecto(double v) { this.porcentajeProyecto = v; }
 
-    public double getPorcentajeActividades() {
-        return this.porcentajeActividades;
-    }
+    public double getCalificacionMinima() { return calificacionMinima; }
+    public void setCalificacionMinima(double v) { this.calificacionMinima = v; }
 
-    public void setPorcentajeActividades(double v) {
-        this.porcentajeActividades = v;
-    }
-
-    public double getPorcentajeProyecto() {
-        return this.porcentajeProyecto;
-    }
-
-    public void setPorcentajeProyecto(double v) {
-        this.porcentajeProyecto = v;
-    }
-
-    public double getCalificacionMinima() {
-        return this.calificacionMinima;
-    }
-
-    public void setCalificacionMinima(double v) {
-        this.calificacionMinima = v;
-    }
-
-    public int getFaltasMaximas() {
-        return this.faltasMaximas;
-    }
-
-    public void setFaltasMaximas(int v) {
-        this.faltasMaximas = v;
-    }
+    public int getFaltasMaximas() { return faltasMaximas; }
+    public void setFaltasMaximas(int v) { this.faltasMaximas = v; }
 }
