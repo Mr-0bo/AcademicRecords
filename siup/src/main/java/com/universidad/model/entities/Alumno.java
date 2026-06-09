caa33ef7d49d24e346840ccacc9da9b11e9cd723
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 16:22:31 -0600

    Update Alumno.java

--- siup/src/main/java/com/universidad/model/entities/Alumno.java
+++ siup/src/main/java/com/universidad/model/entities/Alumno.java
@@ -1,5 +1,7 @@
 package com.universidad.model.entities;
 
+import com.universidad.model.enums.TipoUsuario;
+
 public class Alumno extends Usuario {
     private String matricula;
     private int idCarrera;
