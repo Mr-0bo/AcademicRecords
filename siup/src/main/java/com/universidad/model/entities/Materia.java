4b1f5b43c66a161b8c442b5b5e8297b7d40c6bfb
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 15:37:48 -0600

    Update Materia.java

--- siup/src/main/java/com/universidad/model/entities/Materia.java
+++ siup/src/main/java/com/universidad/model/entities/Materia.java
@@ -5,10 +5,9 @@ public class Materia {
     private String nombre;
     private int idCarrera;
     private int semestre;
-    private String nombreCarrera;
+    private String nombreCarrera; // auxiliar para UI
 
-    public Materia() {
-    }
+    public Materia() {}
 
     public Materia(int id, String nombre, int idCarrera, int semestre) {
         this.id = id;
@@ -17,47 +16,21 @@ public class Materia {
         this.semestre = semestre;
     }
 
-    public int getId() {
-        return this.id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getNombre() {
-        return this.nombre;
-    }
+    public int getIdCarrera() { return idCarrera; }
+    public void setIdCarrera(int idCarrera) { this.idCarrera = idCarrera; }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public int getSemestre() { return semestre; }
+    public void setSemestre(int semestre) { this.semestre = semestre; }
 
-    public int getIdCarrera() {
-        return this.idCarrera;
-    }
+    public String getNombreCarrera() { return nombreCarrera; }
+    public void setNombreCarrera(String nombreCarrera) { this.nombreCarrera = nombreCarrera; }
 
-     public void setIdCarrera(int idCarrera) {
-        this.idCarrera = idCarrera;
-    }
-
-    public int getSemestre() {
-        return this.semestre;
-    }
-
-    public void setSemestre(int semestre) {
-        this.semestre = semestre;
-    }
-
-    public String getNombreCarrera() {
-        return this.nombreCarrera;
-    }
-
-    public void setNombreCarrera(String nombreCarrera) {
-        this.nombreCarrera = nombreCarrera;
-    }
-
-    public String toString() {
-        return this.nombre + " (Sem. " + this.semestre + ")";
-    }
+    @Override
+    public String toString() { return nombre + " (Sem. " + semestre + ")"; }
 }
