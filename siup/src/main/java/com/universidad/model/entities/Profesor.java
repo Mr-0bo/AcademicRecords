08921144fb29c9c9bd04897e51ceb95ddb1cd858
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:31:38 -0600

    Update Profesor.java

--- siup/src/main/java/com/universidad/model/entities/Profesor.java
+++ siup/src/main/java/com/universidad/model/entities/Profesor.java
@@ -1,4 +1,15 @@
 package main.java.com.universidad.model.entities;
 
 public class Profesor {
+  public class Profesor extends Usuario {
+    private String numeroEmpleado;
+
+    public Profesor() {
+        this.setTipo(TipoUsuario.PROFESOR);
+    }
+
+    public Profesor(int id, String nombre, String correo, String contrasena, String numeroEmpleado) {
+        super(id, nombre, correo, contrasena, TipoUsuario.PROFESOR);
+        this.numeroEmpleado = numeroEmpleado;
+    }
 }
