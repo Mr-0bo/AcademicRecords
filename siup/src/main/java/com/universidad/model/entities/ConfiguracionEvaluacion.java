dfffb0e3bdb1c03ac7e28df1fda67111983c9d7d
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:09:47 -0600

    Update ConfiguracionEvaluacion.java

--- siup/src/main/java/com/universidad/model/entities/ConfiguracionEvaluacion.java
+++ siup/src/main/java/com/universidad/model/entities/ConfiguracionEvaluacion.java
@@ -1,4 +1,15 @@
 package main.java.com.universidad.model.entities;
 
 public class ConfiguracionEvaluacion {
+  public class ConfiguracionEvaluacion {
+    private int id;
+    private int idMateria;
+    private double porcentajeParciales;
+    private double porcentajeActividades;
+    private double porcentajeProyecto;
+    private double calificacionMinima;
+    private int faltasMaximas;
+
+    public ConfiguracionEvaluacion() {
+    }
 }
