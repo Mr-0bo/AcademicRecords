e34f2f5ad339644f007dea14a5b08b6270685121
IanSigler <iancarlosigler@gmail.com>
2026-06-09 08:53:30 -0600

    Extend Alumno class from Usuario and add fields

--- siup/src/main/java/com/universidad/model/entities/Alumno.java
+++ siup/src/main/java/com/universidad/model/entities/Alumno.java
@@ -1,4 +1,12 @@
 package main.java.com.universidad.model.entities;
 
 public class Alumno {
+  public class Alumno extends Usuario {
+    private String matricula;
+    private int idCarrera;
+    private String nombreCarrera;
+
+    public Alumno() {
+        this.setTipo(TipoUsuario.ALUMNO);
+    }
 }
