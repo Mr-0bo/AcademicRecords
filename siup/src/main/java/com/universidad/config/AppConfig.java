42458af6cf4df77fdedde957d52ac41bffba52e9
رافايل موراليس <morales.rafa4002@gmail.com>
2026-06-09 10:20:32 -0600

    Update AppConfig.java

--- siup/src/main/java/com/universidad/config/AppConfig.java
+++ siup/src/main/java/com/universidad/config/AppConfig.java
@@ -1,4 +1,14 @@
-package main.java.com.universidad.config;
+package com.universidad.config;
 
+/**
+ * Constantes globales de la aplicación.
+ * Cambiar aquí afecta a todo el sistema.
+ */
 public class AppConfig {
+    public static final String APP_NAME     = "Sistema Universitario de Calificaciones";
+    public static final String VERSION      = "1.0.0";
+    public static final String DB_FILENAME  = "universidad.db";
+    public static final double CALIF_MINIMA = 6.0;
+    public static final int    FALTAS_MAX   = 3;
+    private AppConfig() {}
 }
