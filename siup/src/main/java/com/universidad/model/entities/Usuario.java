b5af23eee433299350b012a6fd14ddba1abc5ebb
IanSigler <iancarlosigler@gmail.com>
2026-06-09 09:33:31 -0600

    Update Usuario.java

--- siup/src/main/java/com/universidad/model/entities/Usuario.java
+++ siup/src/main/java/com/universidad/model/entities/Usuario.java
@@ -1,4 +1,21 @@
 package main.java.com.universidad.model.entities;
 
 public class Usuario {
+  public class Usuario {
+    private int id;
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private TipoUsuario tipo;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombre, String correo, String contrasena, TipoUsuario tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.tipo = tipo;
+    }
 }
