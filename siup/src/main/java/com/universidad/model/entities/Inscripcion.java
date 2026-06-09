4b229252915be8b819abdc0326cccf6f52847579
Mr-0bo <mrobo.idk@gmail.com>
2026-06-09 17:08:20 -0600

    Update Inscripcion.java

--- siup/src/main/java/com/universidad/model/entities/Inscripcion.java
+++ siup/src/main/java/com/universidad/model/entities/Inscripcion.java
@@ -4,11 +4,10 @@ public class Inscripcion {
     private int id;
     private int idAlumno;
     private int idGrupo;
-    private String nombreAlumno;
-    private String matricula;
+    private String nombreAlumno;  // auxiliar
+    private String matricula;     // auxiliar
 
-    public Inscripcion() {
-    }
+    public Inscripcion() {}
 
     public Inscripcion(int id, int idAlumno, int idGrupo) {
         this.id = id;
@@ -16,47 +15,23 @@ public class Inscripcion {
         this.idGrupo = idGrupo;
     }
 
-    public int getId() {
-        return this.id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getIdAlumno() { return idAlumno; }
+    public void setIdAlumno(int idAlumno) { this.idAlumno = idAlumno; }
 
-    public int getIdAlumno() {
-        return this.idAlumno;
-    }
+    public int getIdGrupo() { return idGrupo; }
+    public void setIdGrupo(int idGrupo) { this.idGrupo = idGrupo; }
 
-    public void setIdAlumno(int idAlumno) {
-        this.idAlumno = idAlumno;
-    }
+    public String getNombreAlumno() { return nombreAlumno; }
+    public void setNombreAlumno(String nombreAlumno) { this.nombreAlumno = nombreAlumno; }
 
-    public int getIdGrupo() {
-        return this.idGrupo;
-    }
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
 
-    public void setIdGrupo(int idGrupo) {
-        this.idGrupo = idGrupo;
-    }
-
-    public String getNombreAlumno() {
-        return this.nombreAlumno;
-    }
-
-    public void setNombreAlumno(String nombreAlumno) {
-        this.nombreAlumno = nombreAlumno;
-    }
-
-    public String getMatricula() {
-        return this.matricula;
-    }
-
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
-
+    @Override
     public String toString() {
-        return this.nombreAlumno != null ? this.nombreAlumno : "Inscripción #" + this.id;
+        return nombreAlumno != null ? nombreAlumno : "Inscripción #" + id;
     }
 }
