dda18830d7ce07ffb7bc0e797d25e95e98a00420
رافايل موراليس <morales.rafa4002@gmail.com>
2026-06-09 10:27:20 -0600

    Update BaseRepository.java

--- siup/src/main/java/com/universidad/repository/BaseRepository.java
+++ siup/src/main/java/com/universidad/repository/BaseRepository.java
@@ -1,4 +1,19 @@
-package main.java.com.universidad.repository;
+package com.universidad.repository;
 
-public class BaseRepository {
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interfaz genérica del patrón Repository / DAO.
+ * Todas las entidades comparten este contrato base.
+ *
+ * @param <T>  Tipo de la entidad
+ * @param <ID> Tipo del identificador
+ */
+public interface BaseRepository<T, ID> {
+    List<T> findAll();
+    Optional<T> findById(ID id);
+    T save(T entity);       // INSERT si id==0, UPDATE si id>0
+    boolean delete(ID id);
 }
+
